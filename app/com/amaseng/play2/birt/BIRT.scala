28e9853ba2dd4d488d505c0f55af03d4ffebf3aa
cheeseng <cheeseng@amaseng.com>
2015-11-18 22:13:47 +0800

    Added run report favor that call Run and Render separately, this will help to get pageNumber and totalPage variable correctly.

--- app/com/amaseng/play2/birt/BIRT.scala
+++ app/com/amaseng/play2/birt/BIRT.scala
@@ -97,6 +97,45 @@ class BIRT {
   def generateReport(templateInputStream: InputStream, format: ReportFormat, parametersMap: Map[String, AnyRef], dataMap: Map[String, AnyRef]): InputStream = {
     runReport(engine.openReportDesign(templateInputStream), format, parametersMap, dataMap)
   }
+
+  private def runReportSeparate(runnable: IReportRunnable, format: ReportFormat, parametersMap: Map[String, AnyRef], dataMap: Map[String, AnyRef]): InputStream = {
+
+    import collection.JavaConverters._
+
+    val options = new HTMLRenderOption()
+    val outStream = new ByteArrayOutputStream()
+    options.setOutputStream(outStream)
+    options.setOutputFormat(format.formatString)
+
+    //Create task to run the report - use the task to execute and run the report,
+    val task = engine.createRunTask(runnable)
+    task.setParameterValues(parametersMap.asJava)
+    task.setAppContext(dataMap.asJava)
+
+    val temp = File.createTempFile("amaseng", ".rptdocument")
+
+    //Create rptdocument
+    task.run(temp.getAbsolutePath)
+
+    //Open rptdocument
+    val rptdoc = engine.openReportDocument(temp.getAbsolutePath)
+
+    val rtask = engine.createRenderTask(rptdoc)
+
+    rtask.setAppContext(dataMap.asJava)
+    rtask.setRenderOption(options)
+    rtask.setParameterValues(parametersMap.asJava)
+
+    rtask.render()
+
+    task.close()
+
+    new ByteArrayInputStream(outStream.toByteArray)
+  }
+
+  def generateReportSeparate(templateInputStream: InputStream, format: ReportFormat, parametersMap: Map[String, AnyRef], dataMap: Map[String, AnyRef]): InputStream = {
+    runReportSeparate(engine.openReportDesign(templateInputStream), format, parametersMap, dataMap)
+  }
 }
 
 object BIRT extends BIRT
